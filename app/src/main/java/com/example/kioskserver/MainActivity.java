526f4d251aadb981f5f4342c19c2b382a24b4a67
hyeona <ha.adastra@gmail.com>
2019-05-19 01:46:09 +0900

    하단 액션바 추가(주문대기, 준비중) 옵션 메뉴 추가(영업상태, 메뉴설정, 매장정보, 로그아웃)

--- app/src/main/java/com/example/kioskserver/MainActivity.java
+++ app/src/main/java/com/example/kioskserver/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.kioskserver;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +27,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private FragmentManager fragmentManager;
+    private AcceptFragment acceptFragment;
+    private WaitConfirmFragment waitConfirmFragment;
     Button btnLogout;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (item.getItemId()) {
+                case R.id.navigation_wait_confirm:
+                    transaction.replace(R.id.framelayout, waitConfirmFragment).commitAllowingStateLoss();
+                    return true;
+                case R.id.navigation_accept:
+                    transaction.replace(R.id.framelayout, acceptFragment).commitAllowingStateLoss();
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +59,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        fragmentManager = getSupportFragmentManager();
+        acceptFragment = new AcceptFragment();
+        waitConfirmFragment = new WaitConfirmFragment();
+        fragmentManager = getSupportFragmentManager();
 
-        btnLogout = (Button)findViewById(R.id.btn_logout);
-        btnLogout.setOnClickListener(this);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, waitConfirmFragment).commitAllowingStateLoss();
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
@@ -48,8 +85,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        mAuth.signOut();
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // 메뉴버튼이 처음 눌러졌을 때 실행되는 콜백메서드
+        // 메뉴버튼을 눌렀을 때 보여줄 menu 에 대해서 정의
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        Log.d("test", "onCreateOptionsMenu - 최초 메뉴키를 눌렀을 때 호출됨");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 메뉴의 항목을 선택(클릭)했을 때 호출되는 콜백메서드
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Log.d("test", "onOptionsItemSelected - 메뉴항목을 클릭했을 때 호출됨");
+
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.menu_shop_state:
+                Toast.makeText(getApplicationContext(), "영업 상태",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_set_menu:
+                Toast.makeText(getApplicationContext(), "메뉴 설정",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_set_shop_info:
+                Toast.makeText(getApplicationContext(), "매장 정보",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_logout:
+                new AlertDialog.Builder(this/* 해당 액티비티를 가르킴 */)
+                        .setTitle("로그아웃").setMessage("로그아웃 하시겠습니까?")
+                        .setPositiveButton("로그아웃", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                mAuth.signOut();
+                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                            }
+                        })
+                        .show();
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/kioskserver/WaitConfirmFragment.java
@@ -0,0 +1,26 @@
+package com.example.kioskserver;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class WaitConfirmFragment extends Fragment {
+
+    public WaitConfirmFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_wait_confirm, container, false);
+    }
+
+}
